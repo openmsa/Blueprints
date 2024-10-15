6912c5b163a479ea56b7371148833e105404fa7b
DESKTOP-QMMAEO9\Admin <ddh@ubiqube.com>
2024-10-15 12:11:54 +0530

    Removed Uninstalled urllib command

--- install.sh
+++ install.sh
@@ -9,8 +9,7 @@ ln -fsn ../OpenMSA_Blueprints/Catalog Catalog;
 emit_step "CLI tools (TF, kubectl, AWS CLI, Google SDK...)"
 install_repo_deps.sh /opt/fmc_repository/Process/cloudclapp-wf/
 popd || exit
-emit_step "Uninstalling urllib3 for ZAP scan to run"
-python3 -m pip uninstall -y urllib3
+
 
 pushd /opt/fmc_repository/Process/PythonReference || exit
 emit_step "Symlink to CCLA_WF"
