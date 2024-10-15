485cb6e94e977a9a33b1b1f4bc583698061dcc9b
DESKTOP-QMMAEO9\Admin <ddh@ubiqube.com>
2024-10-15 12:14:32 +0530

    Removed uninstall urllib3

--- install.sh
+++ install.sh
@@ -9,8 +9,6 @@ ln -fsn ../OpenMSA_Blueprints/Catalog Catalog;
 emit_step "CLI tools (TF, kubectl, AWS CLI, Google SDK...)"
 install_repo_deps.sh /opt/fmc_repository/Process/cloudclapp-wf/
 popd || exit
-
-
 pushd /opt/fmc_repository/Process/PythonReference || exit
 emit_step "Symlink to CCLA_WF"
 ln -fsn /opt/fmc_repository/Process/cloudclapp-wf/cloud_sdk ccla_cloud
