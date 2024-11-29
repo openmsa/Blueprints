b4017f5bf3d5d65907c0a10d031aadde90af7c41
Antoine Brun <antoine.brun@gmail.com>
2024-11-29 17:22:02 +0100

    remove the call to CCLA tool installed

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ pushd /opt/fmc_repository/Blueprints || exit
 
 emit_step "Blueprints from OpenMSA_Blueprints"
 ln -fsn ../OpenMSA_Blueprints/Catalog Catalog;
-emit_step "CLI tools (TF, kubectl, AWS CLI, Google SDK...)"
-install_repo_deps.sh /opt/fmc_repository/Process/cloudclapp-wf/
 popd || exit
 pushd /opt/fmc_repository/Process/PythonReference || exit
 emit_step "Symlink to CCLA_WF"
