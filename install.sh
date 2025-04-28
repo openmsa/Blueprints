a21e3a722a9aa3c22047fa845f4cce856851af6e
Antoine Brun <antoine.brun@gmail.com>
2025-04-28 16:22:11 +0200

    small typo fixes

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 . /usr/share/install-libraries/il-lib.sh
 
 pushd /opt/fmc_repository/Blueprints || exit
-
 emit_step "Blueprints from OpenMSA_Blueprints"
 ln -fsn ../OpenMSA_Blueprints/Catalog Catalog;
 popd || exit
+
 pushd /opt/fmc_repository/Process/PythonReference || exit
-emit_step "Symlink to CCLA_WF"
+emit_step "Symlink to CCLA SDK"
 ln -fsn /opt/fmc_repository/Process/cloudclapp-wf/cloud_sdk ccla_cloud
 popd || exit
